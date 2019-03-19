ef0f475f28cf47ea222b99c46c266909d8c4dc59
Chris Moreno-Stokoe <cm14911@bristol.ac.uk>
2019-03-19 16:15:29 +0000

    rollback

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.edges import EdgeList, Edge
 from resources.annot import AnnotList, Annot, Home
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://kcabvtljvflpnc:a1e491d35733f7784b7fc3e3c205a3794b45c3e383abacd398ea0928e326112e@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/d5r274ufjsq4v9','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'authKey01'
 api = Api(app)
